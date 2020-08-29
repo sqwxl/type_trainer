a7dd13e688ca42e64de8874722a4867f4198d930
Nicolas Epstein <nico@nilueps.net>
2020-08-29 17:33:07 -0400

    added event logic; incomplete

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render } from './utils/test-utils';
+import { fireEvent, render, screen } from './utils/test-utils';
 import App from './App'
 
 describe('App', () => {
--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,67 @@ import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import Keyboard from './Components/Keyboard/Keyboard';
 import { TrainerDisplayArea, TrainerDisplayAreaProps } from './Components/TrainerDisplayArea/TrainerDisplayArea';
-import { ThemeContext, themes } from './Components/Providers/ThemeProvider/ThemeContext';
-import { LayoutContext, layouts } from './Components/Providers/LayoutProvider/LayoutContext';
+import { ThemeContext, themes } from './Components/Contexts/ThemeContext/ThemeContext';
+import { LayoutContext, layouts } from './Components/Contexts/LayoutContext/LayoutContext';
 
 
 interface AppProps { }
 interface AppState {
-  displayText: string
+  displayText: string;
+  pressed: string[];
 }
 const INITIALSTATE: AppState = {
-  displayText: ""
+  displayText: "",
+  pressed: []
 }
 
 class App extends React.Component<{}, AppState> {
+  keyReleaseTimer: NodeJS.Timeout;
   constructor(props: AppProps) {
     super(props)
     this.state = INITIALSTATE
-    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleKeyDown = this.handleKeyDown.bind(this)
+    this.keyReleaseTimer = setTimeout(() => { }, 0)
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
   }
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress)
+    document.addEventListener('keydown', this.handleKeyDown)
+    document.addEventListener('keyup', this.handleKeyUp)
+    document.addEventListener('blur', this.handleBlur)
   }
 
-  handleKeyPress(event: KeyboardEvent) {
+  handleKeyDown(event: KeyboardEvent) {
     event.preventDefault()
-    console.dir(event)
+    let pressed = this.state.pressed
+    // if (pressed.includes(event.code)) {
+    //   clearTimeout(this.keyReleaseTimer)
+    //   return
+    // }
+    pressed.push(event.code)
+    this.setState({ pressed: pressed }, () => this.keyReleaseTimer = this.scheduleKeyRelease(event.code))
+  }
+  
+  handleKeyUp(event: KeyboardEvent) {
+    event.preventDefault()
+    let pressed = this.state.pressed.filter(code => code !== event.code)
+    this.setState({ pressed: pressed})
+  }
+
+  scheduleKeyRelease = (c: string) => setTimeout(() => {
+    let pressed = this.state.pressed.filter(code => code !== c)
+    this.setState({ pressed: pressed})
+  }, 50)
+
+  handleBlur() {
+    this.setState({ pressed: [] })
   }
 
   render() {
     return (
       <div className="App" >
         <TrainerDisplayArea displayText={this.state.displayText}></TrainerDisplayArea>
-        <Keyboard />
+        <Keyboard pressed={this.state.pressed} />
       </div>
     );
   }
